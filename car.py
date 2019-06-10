9e6b9661e5fb411ed29723944373348b0975dbb2
Axel Adrián Anaya García <axel_anaya@hotmail.com>
2019-06-10 11:49:39 -0500

    changes to car.py

--- car.py
+++ car.py
@@ -1,15 +1,25 @@
 command = input("> ")
+started = False
 while command.lower() != "quit":
     if command.lower() == "start":
-        print("Car started")
+        if started:
+            print("The car is already started!")
+        else:
+            started = True
+            print("Car started")
     elif command == "help":
         print('''
-        help: Shows the commands
-        start: starts the car
-        stop: stops the car
-        quit: exits the program''')
+help: Shows the commands        
+start: starts the car
+stop: stops the car
+quit: exits the program
+''')
     elif command == "stop":
-        print("Car stopped")
+        if started:
+            started = False
+            print("Car stopped")
+        else:
+            print("The car is already stopped")
     else:
         print("I don't understand...")
     command = input("> ").lower()
